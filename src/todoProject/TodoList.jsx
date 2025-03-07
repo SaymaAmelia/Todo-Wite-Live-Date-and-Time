748a4ce0484e454a8a2bd7853a82f21567335dc0
Sayma Amelia <saymaamelia@gmailcom>
2025-03-07 12:23:02 +0600

    distPart -complete

--- src/todoProject/TodoList.jsx
+++ src/todoProject/TodoList.jsx
@@ -4,8 +4,10 @@ import { MdCheck } from "react-icons/md";
 import { MdDeleteOutline } from "react-icons/md";
 const TodoList = ({data,checked, onHandleDeleteTodo, onHandleCheckedTodo}) => {
   return (
-    <li className='rima w-103 h-10 my-2  flex text-xl ' >
-    <span className={checked?"checkList":"notCheklist"}>{data}</span>
+    <li className='rima w-103 h-10 my-5  flex text-xl ' >
+      <div className='h-11 w-10/12 '>
+           <span className={checked?"checkList":"notCheklist"}>{data}</span>
+      </div>
     <button className='w-1/12   font-extrabold p-1 mr-1' onClick={()=>onHandleCheckedTodo(data)} ><MdCheck /> </button>
     <button className='w-1/12  font-extrabold p-1 mr-1' onClick={()=>onHandleDeleteTodo(data)} ><MdDeleteOutline /></button>
 
